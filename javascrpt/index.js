36009160a668603c6e38eda74fe877cc817f1cf5
Muhammad-Abrar <tahseenbdserver@outlook.com>
2024-02-19 23:27:40 +0600

    added coupon functionality

--- javascrpt/index.js
+++ javascrpt/index.js
@@ -2,8 +2,10 @@ function main(event) {
   const element = event.target;
   const getNumber = document.getElementById("number");
   const getSeatNumber = document.getElementById("seat-number");
-  const totalMoney = document.getElementById("totalMoney")
+  const totalMoney = document.getElementById("totalMoney");
+  const couponInput = document.getElementById("couponInput");
   const link = document.createElement("p");
+
   link.innerHTML = `<ul style="display: flex ;gap:24px; padding: 12px;" id="removeId">
   <li>Seat</li>
   <li>Economy</li>
@@ -18,13 +20,13 @@ function main(event) {
   if (element.classList.contains("bg-green-400")) {
     getNumber.innerText = parseInt(getNumber.innerText) - 1;
     getSeatNumber.innerText = parseInt(getSeatNumber.innerText) + 1;
-    totalMoney.innerText = parseInt(totalMoney.innerText)+550;
+    totalMoney.innerText = parseInt(totalMoney.innerText) + 550;
 
     appender.appendChild(link);
   } else {
     getNumber.innerText = parseInt(getNumber.innerText) + 1;
     getSeatNumber.innerText = parseInt(getSeatNumber.innerText) - 1;
-    totalMoney.innerText = parseInt(totalMoney.innerText)-550;
+    totalMoney.innerText = parseInt(totalMoney.innerText) - 550;
     appender.removeChild(appender.lastElementChild);
   }
 
@@ -32,10 +34,12 @@ function main(event) {
     alert("You can't book more than 4 seats");
     getNumber.innerText = parseInt(getNumber.innerText) + 1;
     getSeatNumber.innerText = parseInt(getSeatNumber.innerText) - 1;
-    totalMoney.innerText = parseInt(totalMoney.innerText)-550;
+    totalMoney.innerText = parseInt(totalMoney.innerText) - 550;
     element.classList.remove("bg-green-400");
     appender.removeChild(appender.lastElementChild);
   }
+
+
 
   return;
 }
@@ -43,4 +47,28 @@ function main(event) {
 var elements = document.getElementsByClassName("Choice");
 for (var i = 0; i < elements.length; i++) {
   elements[i].addEventListener("click", main);
+}
+
+function coupon() {
+  const getNumber = document.getElementById("number");
+  const getSeatNumber = document.getElementById("seat-number");
+  const totalMoney = document.getElementById("totalMoney");
+  const couponInput = document.getElementById("couponInput");
+  const couponBox = document.getElementById("couponBox");
+  const grandTotal = document.getElementById("grandTotal")
+
+  if (couponInput.value === "NEW15") {
+    grandTotal.innerText =
+      parseInt(totalMoney.innerText) - parseInt(totalMoney.innerText) * 0.15;
+
+    couponBox.classList.add("hidden");
+ 
+  }
+
+ if (couponInput.value === "Couple 20") {
+      grandTotal.innerText =
+        parseInt(totalMoney.innerText) - parseInt(totalMoney.innerText) * 0.2;
+
+      couponBox.classList.add("hidden");
+    }
 }
